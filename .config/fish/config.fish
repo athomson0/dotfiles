3aa9b0ac273b3057877a089dfcc0cc7bb12d40ac
Aidan <aidan@good-loop.com>
2021-12-20 15:14:55 +0000

    git aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,13 @@ alias ccat="highlight -O ansi --force"
 alias python="python3"
 alias alacritty="gtk-launch Alacritty"
 
+# git stuff
+alias gs="git status"
+alias gco="git checkout"
+alias gb="git checkout -b "
+alias gaa="git add -A"
+alias gc="git commit -m "
+
 #set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
 #source /usr/share/powerline/bindings/fish/powerline-setup.fish
 #powerline-setup
